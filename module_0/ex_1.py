7ac66b5b5698829b47b5e75391edd398d8de7076
denis.borisov <denis.borisov@hotmail.com>
2020-05-20 22:04:47 +0300

    Updated the algorithm and the code structure.

--- module_0/ex_1.py
+++ module_0/ex_1.py
@@ -1,29 +1,43 @@
+'''
+Задание.
+
+Компьютер загадывает целое число от 1 до 100, и нам нужно написать программу, которая угадывает число
+за как можно меньшее количество попыток.
+'''
+
 import np
 
-def game_core_v2(number):
+def guesswork(number):
     '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
        Функция принимает загаданное число и возвращает число попыток'''
-    count = 1
-    predict = np.random.randint(1, 101)
-    while number != predict:
-        count += 1
-        if number > predict:
-            predict += 1
-        elif number < predict:
-            predict -= 1
-    return count # выход из цикла, если угадали
+
+    trials = 1
+    prediction = np.random.randint(1, 101)
+    while prediction != number:
+        trials += 1
+
+        if prediction < number:
+            prediction -= (prediction - number) // 2
+
+        elif prediction > number:
+            prediction += (number - prediction) // 2
+
+    return trials
 
 
-def score_game(game_core):
+def score_game(guesswork):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
-    count_ls = []
+
+    trials_list = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     random_array = np.random.randint(1, 101, size=(1000))
     for number in random_array:
-        count_ls.append(game_core(number))
-    score = int(np.mean(count_ls))
+        trials_list.append(guesswork(number))
+
+    score = int(np.mean(trials_list))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+
     return score
 
-# запускаем
-score_game(game_core_v2)
+
+score_game(guesswork)
